55a1ec9fe97f9df4bcea9ac27b47f4c9adb8be95
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-04-22 15:52:06 -0300

    Add files via upload

--- Herança/Program.java
+++ Herança/Program.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o raio da circunferência: ");
+        double raio = sc.nextDouble();
+        Circunferencia c = new Circunferencia(raio);
+
+        System.out.print("Digite a base do retângulo: ");
+        double base = sc.nextDouble();
+        System.out.print("Digite a altura do retângulo: ");
+        double altura = sc.nextDouble();
+        Retangulo r = new Retangulo(base, altura);
+
+        System.out.println("\n--- Circunferência ---");
+        c.mostra();
+
+        System.out.println("\n--- Retângulo ---");
+        r.mostra();
+    }
+}
